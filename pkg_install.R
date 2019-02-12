da2a42e0e429908a204b5f290d84ad0437c40604
Mohamed <alibimohamed@gmail.com>
2019-02-12 10:40:39 +0000

    Update pkg_install.R

--- pkg_install.R
+++ pkg_install.R
@@ -1,4 +1,5 @@
 if (!requireNamespace("BiocManager"))
     install.packages("BiocManager")
 BiocManager::install(ask=FALSE)
+BiocManager::install(update=TRUE, ask=FALSE)
 BiocManager::install(c("edgeR","gplots","ggplot2"), ask=FALSE)
